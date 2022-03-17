bb08bb6f45ef1f22c2e2b4e85c998117543d6379
rivernine <rivernine@github.com>
2022-03-17 17:49:44 +0900

    save

--- hardhat/scripts/uniqueAlphaDevV2/deploy.js
+++ hardhat/scripts/uniqueAlphaDevV2/deploy.js
@@ -1,25 +1,25 @@
 async function main() {
-    const [deployer, _] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-    const UniqueAlphaDevV2 = await ethers.getContractFactory("UniqueAlphaDevV2");
-    const uniqueAlphaDevV2 = await UniqueAlphaDevV2.deploy(deployer.address);
-  
-    console.log("UniqueAlphaDevV2 address:", uniqueAlphaDevV2.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+  const [deployer, _] = await ethers.getSigners();
 
-// deploy to hardhat virtual network 
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const UniqueAlphaDevV2 = await ethers.getContractFactory("UniqueAlphaDevV2");
+  const uniqueAlphaDevV2 = await UniqueAlphaDevV2.deploy(deployer.address);
+
+  console.log("UniqueAlphaDevV2 address:", uniqueAlphaDevV2.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// deploy to hardhat virtual network
 // 실행: npx hardhat run scripts/deploy.js
-// 
+//
 // deploy to remote test network
 // 실행: npx hardhat run scripts/deploy.js --network <network-name>
