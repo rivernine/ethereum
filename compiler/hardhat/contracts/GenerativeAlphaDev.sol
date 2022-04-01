fd9952e6734763c58931d40d430458f733671f48
rivernine <rivernine@github.com>
2022-04-01 19:38:38 +0900

    save

--- compiler/hardhat/contracts/GenerativeAlphaDev.sol
+++ compiler/hardhat/contracts/GenerativeAlphaDev.sol
@@ -2,17 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
-contract GenerativeAlphaDev is ERC721URIStorage, EIP712, AccessControl, Ownable {
-    
+contract GenerativeAlphaDev is
+    ERC721URIStorage,
+    ERC2981,
+    EIP712,
+    AccessControl,
+    Ownable
+{
     /* Voucher */
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    string private constant SIGNING_DOMAIN = "GenerativeAlphaDev";
+    string private constant SIGNING_DOMAIN = "VoucherDomain";
     string private constant SIGNATURE_VERSION = "1";
     struct NFTVoucher {
         uint256 tokenId;
@@ -22,28 +28,42 @@ contract GenerativeAlphaDev is ERC721URIStorage, EIP712, AccessControl, Ownable 
 
     /* Requirement */
     uint256 public MAX_SUPPLY;
-    string public baseURI;
+    string public baseTokenURI;
     address public vault;
 
     /* Sale */
     bool public isActive;
     uint256 public price;
 
-    constructor(address minter, uint256 maxSupply, string memory _uri, uint256 _price) 
-        ERC721("GenerativeAlphaDev", "GAD")
-        EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {
-            _setupRole(MINTER_ROLE, minter);
-            MAX_SUPPLY = maxSupply;
-            setBaseURI(_uri);
-            setPrice(_price);
-        }
+    /* Loyalty */
+    address public royaltyAddress;
+    uint96 public royaltyFee;
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _minter,
+        address _royaltyAddress,
+        uint96 _royaltyFee,
+        uint256 _maxSupply,
+        string memory _uri,
+        uint256 _price
+    ) ERC721(_name, _symbol) EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {
+        _setupRole(MINTER_ROLE, _minter);
+        _setDefaultRoyalty(_royaltyAddress, _royaltyFee);
+        royaltyAddress = _royaltyAddress;
+        royaltyFee = _royaltyFee;
+        MAX_SUPPLY = _maxSupply;
+        setBaseURI(_uri);
+        setPrice(_price);
     }
 
-    function setBaseURI(string memory _uri) public onlyOwner {
-        baseURI = _uri;
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
     }
 
     function setVault(address _vault) external onlyOwner {
@@ -58,14 +78,27 @@ contract GenerativeAlphaDev is ERC721URIStorage, EIP712, AccessControl, Ownable 
         price = _price;
     }
 
+    function setRoyaltyAddress(address _royaltyAddress) external onlyOwner {
+        royaltyAddress = _royaltyAddress;
+        _setDefaultRoyalty(royaltyAddress, royaltyFee);
+    }
+
+    function setRoyaltyFee(uint96 _royaltyFee) external onlyOwner {
+        royaltyFee = _royaltyFee;
+        _setDefaultRoyalty(royaltyAddress, royaltyFee);
+    }
+    
     /* Redeem */
     function redeem(NFTVoucher calldata voucher) external payable {
         require(isActive, "Sale must be active to mint");
         require(voucher.tokenId + 1 <= MAX_SUPPLY, "Reached max supply.");
-        require(tx.origin == msg.sender, 'The caller is another contract.');
-        require(msg.value >= price, 'Need to send more ETH.');
+        require(tx.origin == msg.sender, "The caller is another contract.");
+        require(msg.value >= price, "Need to send more ETH.");
         address signer = _verify(voucher);
-        require(hasRole(MINTER_ROLE, signer), "Signature invalid or unauthorized");
+        require(
+            hasRole(MINTER_ROLE, signer),
+            "Signature invalid or unauthorized"
+        );
 
         _safeMint(msg.sender, voucher.tokenId);
         _setTokenURI(voucher.tokenId, voucher.tokenURI);
@@ -73,24 +106,39 @@ contract GenerativeAlphaDev is ERC721URIStorage, EIP712, AccessControl, Ownable 
 
     /* Send balance of contract to address referenced in `vault` */
     function withdraw() external payable onlyOwner {
-        require(vault != address(0), 'Vault Invalid');
+        require(vault != address(0), "Vault Invalid");
         require(payable(vault).send(address(this).balance));
     }
 
     /* Verifies the signature for a given Voucher. */
-    function _verify(NFTVoucher calldata voucher) internal view returns (address) {
+    function _verify(NFTVoucher calldata voucher)
+        internal
+        view
+        returns (address)
+    {
         bytes32 digest = _hash(voucher);
         return ECDSA.recover(digest, voucher.signature);
     }
 
     /* Returns a hash of the given Voucher */
-    function _hash(NFTVoucher calldata voucher) internal view returns (bytes32) {
-        return _hashTypedDataV4(keccak256(abi.encode(
-            keccak256("NFTVoucher(uint256 tokenId, string tokenURI)"),
-            voucher.tokenId,
-            keccak256(bytes(voucher.tokenURI))
-        )));
-    } 
+    function _hash(NFTVoucher calldata voucher)
+        internal
+        view
+        returns (bytes32)
+    {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "NFTVoucher(uint256 tokenId, string tokenURI)"
+                        ),
+                        voucher.tokenId,
+                        keccak256(bytes(voucher.tokenURI))
+                    )
+                )
+            );
+    }
 
     /* Returns the chain id of the current blockchain. */
     function getChainID() external view returns (uint256) {
@@ -102,8 +150,16 @@ contract GenerativeAlphaDev is ERC721URIStorage, EIP712, AccessControl, Ownable 
     }
 
     /* For ERC-165 */
-    function supportsInterface(bytes4 interfaceId) public view virtual override (ERC721, AccessControl) returns (bool) {
-        return ERC721.supportsInterface(interfaceId) || AccessControl.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC2981, AccessControl)
+        returns (bool)
+    {
+        return
+            ERC721.supportsInterface(interfaceId) ||
+            ERC2981.supportsInterface(interfaceId) ||
+            AccessControl.supportsInterface(interfaceId);
     }
-
 }
--- generative_alpha.md
+++ generative_alpha.md
@@ -47,3 +47,277 @@ reveal 기능을 사용하려면 메타데이터의 조건은 다음과 같다.
 3. 입력한 민팅 수량만큼 tokenId와 reveal 전의 uri를 voucher에 담아 생성한다.
 4. mint(voucher)
 5. reveal 시점에 baseuri를 변경한다.
+
+## 1. 메타데이터 준비
+1. hashlips를 이용하여 image, metadata, mappingdata 생성
+2. image를 IPFS에 업로드
+3. metadata 수정 후 IPFS에 업로드
+
+### 1.1. Hashlips
+Hashlips을 이용하여 random image, metadata json을 생성한다.
+파일명을 `sha1(prefix + tokenId)`로 해쉬하여 난수화하였다.
+또한 tokenId와 매핑되는 파일 명들의 정보를 별도로 저장하였다.
+
+- `mappingData.json`
+```json
+{
+    "1": "32219ca53a",
+    "2": "b757255117",
+    "3": "9dc3ce44b9",
+    "4": "c870c1291f",
+    "5": "c4594473bb",
+    "6": "f11c6e7db8",
+    "7": "e11c09630d",
+    "8": "6d073f0fa2",
+    "9": "379791bb6d",
+    "10": "c04507c476",
+    "11": "9c65ca72a1",
+    "12": "975e93b4f8",
+    "13": "f9bac46f24",
+    "14": "dd93e86786",
+    "15": "e583f6e911",
+    "16": "344c659042",
+    "17": "d3a703c92c",
+    "18": "02ab08cde6",
+    "19": "c7e2d0588e",
+    "20": "7110111943"
+}
+```
+
+### 1.2. Image를 IPFS에 업로드
+- 이미지 업로드 결과
+  - image baseURI: `ipfs://QmXapx92HYCLkEW8L6WVgPuJQkMBKAzyEbYfPLVYfBL6Ej/`
+```json
+{
+    "Name": "02ab08cde6.png",
+    "Hash": "QmbUn3WbydDNP5ss59s5Hirj9ANRd2kjnH19ALNbayKgTJ",
+    "Size": "223666"
+}
+{
+    "Name": "6d073f0fa2.png",
+    "Hash": "QmTT5Gp5WkUL77eaU2Tk71MKugjha1Bdc3f8EEb1fN7HA1",
+    "Size": "207171"
+}
+{
+    "Name": "9c65ca72a1.png",
+    "Hash": "Qmao2kFXXuECHxLCfXa4y1ydVt3hrRP1jWwH51BgU2QpNt",
+    "Size": "212756"
+}
+{
+    "Name": "9dc3ce44b9.png",
+    "Hash": "QmSVphgoWmTePNJq2nEGAbx2NmeSHMd8kc2FVZRVbdRcrT",
+    "Size": "215007"
+}
+{
+    "Name": "344c659042.png",
+    "Hash": "QmfJGfLy6LwmkAQFfntvnZoQGi72HQLhzo1sM5xNiEWcLC",
+    "Size": "213030"
+}
+{
+    "Name": "975e93b4f8.png",
+    "Hash": "QmRt3Rd4FjzsnAw1ukNe1xUNZbVtSedcyxK7DGooo8J3km",
+    "Size": "207221"
+}
+{
+    "Name": "32219ca53a.png",
+    "Hash": "QmadEW5kDukKEWv1oVuBsDkZatvAaDzAH6DmoPKmQQmPuW",
+    "Size": "223442"
+}
+{
+    "Name": "379791bb6d.png",
+    "Hash": "QmXtkvriFQuZHMswdCCkatNmz1woKR5ViW4oNhKETj5XN1",
+    "Size": "192015"
+}
+{
+    "Name": "7110111943.png",
+    "Hash": "QmP5tTE6HG3Noypn3H6ZrspMWENYTcjisfXH5ix8DMwFg6",
+    "Size": "225353"
+}
+{
+    "Name": "b757255117.png",
+    "Hash": "QmR8wHcyHNyh34tkAUcW2pCczjmX8UFMHi8yf7gXbD91uQ",
+    "Size": "213370"
+}
+{
+    "Name": "c7e2d0588e.png",
+    "Hash": "QmX4ez89Q39MTG8jtDfPtUxrrX9MgY17Hj8f4cN9Qe37x7",
+    "Size": "230221"
+}
+{
+    "Name": "c870c1291f.png",
+    "Hash": "QmTnTdWZZsay5yaXmxTFsRL1TKDmx6hAWzPua6BKJbE6Si",
+    "Size": "199059"
+}
+{
+    "Name": "c04507c476.png",
+    "Hash": "QmVVQfgggha37KoCmRquDTdKzL1h5TKibAkKt7bsG1zykV",
+    "Size": "188158"
+}
+{
+    "Name": "c4594473bb.png",
+    "Hash": "QmcXm1UJ7bct7b1Uf8mc42o3za8XFyswFYBjx4fdkhRWsf",
+    "Size": "224673"
+}
+{
+    "Name": "d3a703c92c.png",
+    "Hash": "QmYnsm3jczrXJiLzCc9JCqV5836VN5ZpHqvfjmeEsxYY8C",
+    "Size": "193090"
+}
+{
+    "Name": "dd93e86786.png",
+    "Hash": "Qmbg25nDKGK39JzQ8zeniLaXUWbi8PU25TLKL8R8F884td",
+    "Size": "211777"
+}
+{
+    "Name": "e11c09630d.png",
+    "Hash": "QmVidFJpzYL72CCp661LMRurbik1TDfedLaPWLm2KkJUCC",
+    "Size": "203452"
+}
+{
+    "Name": "e583f6e911.png",
+    "Hash": "QmRV7MoS4wgfQDvGTrkG64pFcoAuevF7MNJxENDyPWWMNH",
+    "Size": "225209"
+}
+{
+    "Name": "f9bac46f24.png",
+    "Hash": "QmZ5eV7mw9QYJPbDTZoEDipzVQRcF2UEQ27iHBz9gV5hgZ",
+    "Size": "218310"
+}
+{
+    "Name": "f11c6e7db8.png",
+    "Hash": "QmdxRVS6wumVQZsVWZrtXRkGLE2yx95KtVCVb43rsmSRbb",
+    "Size": "197765"
+}
+{
+    "Name": "",
+    "Hash": "QmXapx92HYCLkEW8L6WVgPuJQkMBKAzyEbYfPLVYfBL6Ej",
+    "Size": "4225909"
+}
+```
+
+### 1.3. Metadata 수정 후 IPFS에 업로드
+- 메타데이터 업로드 결과
+  - metadata baseURI: `ipfs://QmQbRroNMsDZuYEG3w6ZfJa8PA8EpaQdAJVMZ8MMZQFXWs/`
+```json
+{
+    "Name": "02ab08cde6.json",
+    "Hash": "QmbuZiN2Ba3JsM23J47iAmNhBx2c3TKkyZS9dvCW2MwVVm",
+    "Size": "817"
+}
+{
+    "Name": "6d073f0fa2.json",
+    "Hash": "QmVeyHNbTTv9HYTACiE5k9yquJrxiEeuVPzNiMBRzgazgp",
+    "Size": "819"
+}
+{
+    "Name": "9c65ca72a1.json",
+    "Hash": "QmXDR97VXzPZobos8wkAZ8hfLYcktFmorshW3YyLZoUDKr",
+    "Size": "819"
+}
+{
+    "Name": "9dc3ce44b9.json",
+    "Hash": "QmXRWCE2B3CaesLvMhWbtXZvnX82gVFbDSd8wDYcwhFxoN",
+    "Size": "814"
+}
+{
+    "Name": "344c659042.json",
+    "Hash": "QmZyBA9KmcS4UymnPRkNGuyUtc9pZihKgxzkomPcQntNCX",
+    "Size": "819"
+}
+{
+    "Name": "975e93b4f8.json",
+    "Hash": "QmXt47VoXtMALBSmP5QBfGkEbfD5qvRkS1CXqR65pXSTkn",
+    "Size": "823"
+}
+{
+    "Name": "32219ca53a.json",
+    "Hash": "QmRK1pFcZsPVZfxcatHFz6qLZmPbWgcuiNKfUP3ZL53JJF",
+    "Size": "817"
+}
+{
+    "Name": "379791bb6d.json",
+    "Hash": "QmSAk2MwGTG3pe6BNfPANmf5rGQHQi2TpYxr556qa9UAu4",
+    "Size": "817"
+}
+{
+    "Name": "7110111943.json",
+    "Hash": "QmdU4DtBBN8xnoBUmiqhXmYfgxyYgQ8HVcrJ1G3PVmgLcT",
+    "Size": "817"
+}
+{
+    "Name": "b757255117.json",
+    "Hash": "QmTqukXLtSUWXLeaSPkSpsPBguZb2qZvTugEng8mXJ3dAN",
+    "Size": "820"
+}
+{
+    "Name": "c7e2d0588e.json",
+    "Hash": "QmU8RCAdLudgzSkyCPKNUdF5pajNZvMHZ26SM4uZXJGUj8",
+    "Size": "814"
+}
+{
+    "Name": "c870c1291f.json",
+    "Hash": "QmW2UpXmE8tYVM7HPozY1Wdapsufgtu9Nn2dUT5qTYhaUg",
+    "Size": "814"
+}
+{
+    "Name": "c04507c476.json",
+    "Hash": "QmX5gWJdUkVo5b2TtRf9XnJbvcR4yzew7KfVS2kCFkSK73",
+    "Size": "820"
+}
+{
+    "Name": "c4594473bb.json",
+    "Hash": "Qmap5dyifRcebr5A2uRxLkabDifESXnoWPYBno8jobESkg",
+    "Size": "817"
+}
+{
+    "Name": "d3a703c92c.json",
+    "Hash": "QmV17T3NeiyrBk2vkpV5NQtJVVCoxT5cnPEJsoteaKfN81",
+    "Size": "820"
+}
+{
+    "Name": "dd93e86786.json",
+    "Hash": "QmRUbK7icsWJSfq1f1xC4GjCxiJEAECrfFdApfCThNZcL1",
+    "Size": "819"
+}
+{
+    "Name": "e11c09630d.json",
+    "Hash": "Qmeu9wqD6mswNA2wGHBZSnURaRfnPoH7mwVToitmwRAESp",
+    "Size": "817"
+}
+{
+    "Name": "e583f6e911.json",
+    "Hash": "QmcKzxeFy7MZZsvxCZX8JR8Xtmc8PERubV7AK8EhNSCjij",
+    "Size": "818"
+}
+{
+    "Name": "f9bac46f24.json",
+    "Hash": "QmYWSK1KztEBr723m2gnXkt3dE8snCqCTqsXMiPWnxbRpD",
+    "Size": "814"
+}
+{
+    "Name": "f11c6e7db8.json",
+    "Hash": "QmbbsZcbAtDzN1fvRE1gBLHBCSNh1ZtJUHFp5cMcn987fj",
+    "Size": "820"
+}
+{
+    "Name": "",
+    "Hash": "QmQbRroNMsDZuYEG3w6ZfJa8PA8EpaQdAJVMZ8MMZQFXWs",
+    "Size": "17519"
+}
+```
+
+## 2. 컨트랙트 배포
+### 2.0. Contract 생성
+Shell script 이용. (생략)
+
+### 2.1. Compile
+Hardhat을 이용하여 컨트랙트 컴파일
+ABI, Bytecode를 획득
+
+### 2.2. Deploy
+ABI와 Bytecode를 application에 전달하고 메타마스크를 활용하여 배포 진행
+배포 후 contract address 획득
+e.g. `0x171989A4080bCEc8B4C67f49699c33311bCF116E`
+
+### 3. Voucher 발급
+### 4. 민팅
